bb99a11993e756f40bc74b544e11acc7d4e2351f
Krishna Nautiyal <getkrishnanautiyal@gmail.com>
2025-04-22 22:41:37 +0530

    defined toggleSlide function and initialising first slide

--- js-files/slider.js
+++ js-files/slider.js
@@ -1,3 +1,11 @@
 const slides = document.querySelectorAll('.slide');
 const navItems = document.querySelectorAll('.nav-item');
 
+function toggleSlide(index) {
+    slides.style.transform = `translateX(-${index * 100}%)`;
+    navItems.forEach((item, i) => {
+        item.classList.toggle('active', i === index);
+    });
+}
+
+toggleSlide(0);
