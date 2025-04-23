0dcc5e6023b95e3c04caede269d7f9abe142d0c4
codershivam96 <thessinghania@gmail.com>
2025-04-23 09:32:11 +0530

    solving issue in hamburger

--- js-files/hamburger-menu.js
+++ js-files/hamburger-menu.js
@@ -1,6 +1,9 @@
-const hamMenu = document.querySelector('.ham-menu');
-const offScreenMenu = document.querySelector('.off-screen-menu');
-hamMenu.addEventListener('click', () => {
-  hamMenu.classList.toggle('active');
-  offScreenMenu.classList.toggle('active');
+document.addEventListener('click', (e) => {
+  const hamMenu = document.querySelector('.ham-menu');
+  const offScreenMenu = document.querySelector('.off-screen-menu');
+
+  if (e.target.closest('.ham-menu')) {
+    hamMenu.classList.toggle('active');
+    offScreenMenu.classList.toggle('active');
+  }
 });
